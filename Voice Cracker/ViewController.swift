4b830032008d7e9270e5a77816b5c54bd8cd87fe
Gayan Jayasundara <gayanjayasundara@bdpc00132.bluedrop.local>
2015-09-17 12:32:59 -0230

    Initial project creation

---
+++ Voice Cracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice Cracker
+//
+//  Created by Gayan Jayasundara on 2015-09-17.
+//  Copyright © 2015 Gayan Jayasundara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
